297e57a46b1fdc115b8c2cb5273e8ca43e402d44
Knud van Eeden <knud_van_eeden@hyahoo.com>
2022-12-12 14:14:04 +0100

    draft

--- compileall.bat
+++ compileall.bat
@@ -1,8 +1,12 @@
-@REM 1. You have to replace the path to your TSE sc32.exe to that on your system
+@REM 1. Download and unzip all these TSE macros
 @REM
-@REM 2. Then put this .bat file in the root directory of your unzipped download file and run it
+@REM     https://github.com/knudvaneeden/ddd/archive/refs/heads/TRUNK.zip
 @REM
-@REM 3. It will then automatically recompile all .s and .si in all the TSE macros
+@REM 2. You have to replace the path to your TSE sc32.exe to that on your system
+@REM
+@REM 3. Then put this .bat file in the root directory of your unzipped download file and run it
+@REM
+@REM 4. It will then automatically recompile all .s and .si in all the TSE macros for your current TSE version (e.g. 4.48 or higher)
 @REM
 for /R %%f in (*.s) DO f:\wordproc\tse32_v44200\sc32.exe %%f
 @REM
