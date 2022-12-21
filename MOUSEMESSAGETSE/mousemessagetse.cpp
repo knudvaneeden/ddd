07471d9cacd44a6b64646e84be94731510176384
Knud van Eeden <knud_van_eeden@hyahoo.com>
2022-12-21 03:12:05 +0100

    draft

--- MOUSEMESSAGETSE/mousemessagetse.cpp
+++ MOUSEMESSAGETSE/mousemessagetse.cpp
@@ -3,19 +3,40 @@
 //
 #include <windows.h>
 //
+#include <winuser.h>
+//
+#include <stdio.h>
+//
 LRESULT CALLBACK WindowProc( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam ) {
+ //
+ while ( 1 ) {
+ //
  switch ( uMsg ) {
   //
-  case WM_SIZE: {
+  case WM_MOUSEWHEEL: {
+   //
+   // int width = LOWORD( lParam ); // Macro to get the low-order word.
+   // int height = HIWORD( lParam ); // Macro to get the high-order word.
+   //
+   printf( "mousewheel was rotated\n" );
+   //
+   // Respond to the message:
+   //
+  }
+  case WM_MOUSEHWHEEL: {
    //
    int width = LOWORD( lParam ); // Macro to get the low-order word.
    int height = HIWORD( lParam ); // Macro to get the high-order word.
+   //
+   printf( "mousewheel was tilted\n" );
    //
    // Respond to the message:
    //
   }
   break;
  }
+ }
+ return DefWindowProc( hwnd, uMsg, wParam, lParam );
 }
 //
 void main() {
